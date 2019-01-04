122282acd42608c5da337501f2da7d6a789400e6
Cody Taft <cody.taft@gmail.com>
2019-01-04 14:13:49 -0700

    Make apiCalls separate file

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -141,34 +141,3 @@ function onSaveClick() {
   let dream = document.getElementById('query').value;
   saveDreamToDatabase(date, dream);
 }
-
-//api calls
-
-saveDreamToDatabase = async (date, dream) => {
-  try {
-    const url = `http://localhost:3446/api/v1/dreams`;
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        date: date,
-        dream: dream
-      })
-    });
-    const newDream = await response.json();
-    return await newDream;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getAllDreams = async () => {
-  try {
-    const url = `http://localhost:3446/api/v1/dreams`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return await data;
-  } catch (error) {
-    console.log(error);
-  }
-};
