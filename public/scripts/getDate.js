76be4458822fb4e09429f415fb903fbfce5bee16
Cody Taft <cody.taft@gmail.com>
2019-01-03 20:09:48 -0700

    Change var to let in getDate file

--- public/scripts/getDate.js
+++ public/scripts/getDate.js
@@ -1,11 +1,11 @@
-var date = new Date();
+let date = new Date();
 
-var day = date.getDate();
-var month = date.getMonth() + 1;
-var year = date.getFullYear();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
 
 if (month < 10) month = '0' + month;
 if (day < 10) day = '0' + day;
 
-var today = year + '-' + month + '-' + day;
+let today = year + '-' + month + '-' + day;
 document.getElementById('date-input').value = today;
