2b39fa0e09223c326174c98f0bf8299359094342
Cody Taft <cody.taft@gmail.com>
2019-01-07 00:03:55 -0700

    Add users to seed file

--- db/seeds/dreams.js
+++ db/seeds/dreams.js
@@ -1,23 +1,34 @@
 const dreams = require('../dreams/newDreams.json');
 
-const createDream = (knex, dream) => {
+const createDream = (knex, dream, user) => {
   return knex('dreams').insert(
     {
       date: dream.date,
-      dream: dream.dream
+      dream: dream.dream,
+      user_id: user
     },
-    ['id', 'date', 'dream']
+    ['id', 'date', 'dream', 'user_id']
+  );
+};
+
+const createUser = knex => {
+  return knex('users').insert(
+    { user_name: 'Cody Taft', user_token: '103789457650925905775' },
+    'id'
   );
 };
 
 exports.seed = function(knex, Promise) {
   return knex('dreams')
     .del()
+    .then(() => knex('users').del())
     .then(() => {
+      return createUser(knex);
+    })
+    .then(user => {
       let dreamPromises = [];
-
       dreams.forEach(dream => {
-        dreamPromises.push(createDream(knex, dream));
+        dreamPromises.push(createDream(knex, dream, user[0]));
       });
       return Promise.all(dreamPromises);
     })
