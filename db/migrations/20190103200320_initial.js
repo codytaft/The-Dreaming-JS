0f0536474b25c52365d2733b879f16c7f6036551
Cody Taft <cody.taft@gmail.com>
2019-01-07 20:09:20 -0700

    Change ids to user_id and dream_id

--- db/migrations/20190103200320_initial.js
+++ db/migrations/20190103200320_initial.js
@@ -1,12 +1,12 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('users', table => {
-      table.increments('id').primary();
+      table.increments('user_id').primary();
       table.string('user_name');
       table.string('user_token');
     }),
     knex.schema.createTable('dreams', table => {
-      table.increments('id').primary();
+      table.increments('dream_id').primary();
       table.text('dream');
       table.date('date');
       table.integer('user_id').unsigned();
