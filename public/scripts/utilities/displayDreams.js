52e65182e11ef2b634ba74d8d17b3cd786f42ebc
Cody Taft <cody.taft@gmail.com>
2019-01-08 14:18:00 -0700

    Separate display dreams into its own file

--- public/scripts/utilities/displayDreams.js
+++ public/scripts/utilities/displayDreams.js
@@ -1,11 +1,16 @@
-// $(document).ready(function() {
-//   dreamsPromise.then(dreams => {
-//     dreams.forEach(dream => {
-//       $('.dream-list').prepend(`
-//         <li id=${dream.id} class="dream-list-item">
-//           <h4>${dream.date.slice(0, 10)}</h4>
-//           <p>${dream.dream}</p>
-//           `);
-//     });
-//   });
-// });
+displayDreams = dreams => {
+  console.log(dreams);
+  $(document).ready(function() {
+    $('.dream-list').empty();
+    dreams.forEach(dream => {
+      $('.dream-list').prepend(`
+          <li id=${dream.dream_id} class="dream-list-item">
+            <h4 class="dream-date">${dream.date.slice(
+              0,
+              10
+            )}<i class="far fa-trash-alt" onclick="onTrashClick(event)"></i></h4>
+            <p class="dream-text">${dream.dream}</p>
+            `);
+    });
+  });
+};
